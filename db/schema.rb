194d5dcf9eb8db31d744db3f3f3380eb4b572a21
github username <kelly.hesterly@gmail.com>
2022-01-17 22:18:44 -0700

    commit tree f6d394d451c6c45e1b07eeaefa1288316683b896

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140212205242) do
+ActiveRecord::Schema.define(version: 20211022175232) do
 
   create_table "passengers", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -18,8 +18,8 @@ ActiveRecord::Schema.define(version: 20140212205242) do
   end
 
   create_table "rides", force: :cascade do |t|
-    t.integer  "passenger_id"
     t.integer  "taxi_id"
+    t.integer  "passenger_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
   end
